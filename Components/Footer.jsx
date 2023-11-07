7b667fa243db611cf269d7f9975cc2888e38ee6a
aleber123 <alexander.bergqvist@gmail.com>
2023-11-07 14:52:39 +0100

    Add Test component and update test component

---
+++ Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-content">
+        <p>&copy; 2023 Din Företagsnamn</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
